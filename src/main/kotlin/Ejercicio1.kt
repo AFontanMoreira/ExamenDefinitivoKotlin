d9f1cf7e96a2706e5833901fe7b5c70548501403
AFontanMoreira <afontanmoreira@danielcastelao.org>
2021-06-06 17:08:01 +0200

    Ejercicio 1

---
+++ src/main/kotlin/Ejercicio1.kt
@@ -0,0 +1,8 @@
+class Ejercicio1(size: Int) {
+
+    var contador = 0
+
+    var tabla7 = Array(size){it * 7}
+
+
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,14 @@
+import Ejercicio1 as Ejercicio1
+
 fun main() {
-    
+    /**
+     * Apartado1
+     */
+    var contador = 0
+    for (resultado in Ejercicio1(11).tabla7){
+        println("7 x $contador = $resultado")
+        contador++
+    }
+
+
 }
