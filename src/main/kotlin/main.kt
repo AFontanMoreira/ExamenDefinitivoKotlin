cbe3a7d1467088f7b618cbd1db365da6ab9300c3
Alex <72071936+AFontanMoreira@users.noreply.github.com>
2021-06-06 17:20:37 +0200

    Ejercicio 2 (#2)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
+import kotlin.random.Random
 import Ejercicio1 as Ejercicio1
 
 fun main() {
@@ -7,6 +8,20 @@ fun main() {
     var contador = 0
     for (resultado in Ejercicio1(11).tabla7){
         println("7 x $contador = $resultado")
+        contador++
+    }
+    /**
+     * Apartado2
+     */
+
+    println("----------------------- Apartado 2 -----------------------")
+
+    contador = 1
+    val numerosAleaorios = Ejercicio2(10){
+        Random.nextInt(50,250)
+    }
+    numerosAleaorios.aleatorios.forEach {
+        println("$contador = $it")
         contador++
     }
 
